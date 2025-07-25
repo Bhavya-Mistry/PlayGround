e6d6d28417f275a14dd69acf1846d6915840bd26
Bhavya-Mistry <mistrybhavya9@gmail.com>
2025-07-25 11:27:22 +0530

    update

--- LangChain/test.py
+++ LangChain/test.py
@@ -7,7 +7,7 @@ api_key = os.getenv("API_KEY")
 
 
 def generate_pet_name():
-    llm = ChatGoogleGenerativeAI(model="models/gemini-1.5-flash", temperature=0.8, google_api_key=api_key)
+    llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-pro", temperature=0.8, google_api_key=api_key)
     prompt = "I have a dog pet and I want a cool name for it. Suggest me five cool names for my pet."
     name = llm.invoke(prompt)
     return name
