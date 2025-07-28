58b404462a6dcc9eb6aeb5a79409a142da9e0ce7
Bhavya-Mistry <mistrybhavya9@gmail.com>
2025-07-28 19:24:49 +0530

    Update two.py

--- test-projects/two.py
+++ test-projects/two.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv(r"career-navigator\v2\data\dataset.csv")
+df = pd.read_csv(r"career-navigator\v2\data\generated_career_data.csv")
 
 print(df['Recommended_Career'].unique())
