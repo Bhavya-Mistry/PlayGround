b57cca7d753ad1fc7047a43a175e043127a63d45
Bhavya-Mistry <mistrybhavya9@gmail.com>
2025-08-18 14:48:27 +0530

    Update models.py

--- basic-crud/backend/models.py
+++ basic-crud/backend/models.py
@@ -1,7 +1,7 @@
 from extensions import db, bcrypt
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'usertable'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
