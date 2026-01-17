14d6c46e53ec082a7e749c9abd47b40e91ebe3fc
Bhavya-Mistry <mistrybhavya9@gmail.com>
2026-01-17 12:19:26 +0530

    cleaned

--- project/test.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("project/indian_food.csv")
-print(df.columns)
--- test-projects/two.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"career-navigator\v2\data\generated_career_data.csv")
-
-print(df['Recommended_Career'].unique())
